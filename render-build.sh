cb0f622e5f48a80d859bbcdf8d6d7a9545f9ca41
ramshiya <ckramshiyaa@gmail.com>
2025-11-27 13:27:18 +0530

    update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -4,3 +4,7 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
+
+if [ "${RUN_SUPERUSER}" = "1" ]; then
+    python manage.py create_default_superuser || true
+fi
